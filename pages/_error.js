22258de9e551a9d69d3f64e682922159e569fc6f
Neel Redkar <neel.redkar@outlook.com>
2019-09-05 12:41:04 -0700

    add error page

---
+++ pages/_error.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+
+const Error = ()=> {
+    return(
+        <Layout>
+            <h1 className="title">Whoops!</h1>
+            <p>This is awkward... Seems like an error occured....</p>
+            <p>Click the top "My Notebook" to go back!</p>
+        </Layout>
+    )
+}
+
+
+export default Error;
