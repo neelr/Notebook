df1e3b39f75c716052a1be7f158041ecb96e5520
Neel Redkar <neel.redkar@outlook.com>
2019-09-14 00:10:47 -0700

    add https

--- pages/stories/link.js
+++ pages/stories/link.js
@@ -8,7 +8,7 @@ const Flagship = () => {
                     <h1 className="title">LinkShort</h1>
                     <img src="../../static/link.png"  className="storyImage"/>
                     <p style={{color:"#2970f2"}}>9/13/19</p>
-                    <p className="para">Quick update! I made a cool link shortener with airtable as a database! My first real "project" with it and I love how simple it is! Check it out at <a style={{textDecoration: "none",color:"#2970f2"}} href="http://s.neelr.dev">http://s.neelr.dev</a></p>
+                    <p className="para">Quick update! I made a cool link shortener with airtable as a database! My first real "project" with it and I love how simple it is! Check it out at <a style={{textDecoration: "none",color:"#2970f2"}} href="https://s.neelr.dev">https://s.neelr.dev</a></p>
                 </div>
             </div>
         </Layout>
