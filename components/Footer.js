b1ab1a04b39e16ad4b3925c6f67b5225ec31743e
Neel Redkar <35831013+neelr@users.noreply.github.com>
2019-11-01 19:14:49 -0700

    Update username

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = ()=> {
         <div className="footer">
             <a href="https://github.com/hacker719/notebook" className="item" style={{margin:"auto",textDecoration:"none"}}>@Hacker719/Notebook</a>
             <div style={{margin:"auto",display:"flex", color:"#2970f2"}}>
-                <a href="https://github.com/hacker719"><FaGithub className="item" size="1.5em" style={{margin:"10px"}}/></a>
+                <a href="https://github.com/neelr"><FaGithub className="item" size="1.5em" style={{margin:"10px"}}/></a>
                 <a href="mailto:neel.redkar@outlook.com"><FaEnvelope className="item" style={{margin:"10px"}} size="1.5em"/></a>
                 <a href="https://neelr.dev/"><FaLink className="item" style={{margin:"10px"}} size="1.5em"/></a>
             </div>
