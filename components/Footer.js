19a4212c8e40d999debd0a75ef3f105ff4da8d85
Neel Redkar <neel.redkar@outlook.com>
2019-11-19 20:37:13 -0800

    add neelr

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import {FaGithub,FaLink,FaEnvelope} from "react-icons/fa"
 const Footer = ()=> {
     return(
         <div className="footer">
-            <a href="https://github.com/hacker719/notebook" className="item" style={{margin:"auto",textDecoration:"none"}}>@Hacker719/Notebook</a>
+            <a href="https://github.com/neelr/notebook" className="item" style={{margin:"auto",textDecoration:"none"}}>@neelr/Notebook</a>
             <div style={{margin:"auto",display:"flex", color:"#2970f2"}}>
                 <a href="https://github.com/neelr"><FaGithub className="item" size="1.5em" style={{margin:"10px"}}/></a>
                 <a href="mailto:neel.redkar@outlook.com"><FaEnvelope className="item" style={{margin:"10px"}} size="1.5em"/></a>
