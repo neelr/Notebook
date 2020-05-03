7037796b9103881c6e7dbf3e3654bf4c83800bcc
Neel Redkar <neel.redkar@outlook.com>
2020-05-03 04:52:33 -0700

    add various updates

--- pages/stories/[id].js
+++ pages/stories/[id].js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { renderToString } from 'react-dom/server'
 import { Client } from '../../prismic-configuration'
-import Prismic from "prismic-javascript"
+import marked from "marked"
 import { Text, Flex, Heading, Image, Link as RebassLink } from "rebass";
 import Link from "next/link"
 import { RichText } from "prismic-reactjs"
@@ -36,6 +37,9 @@ export default class Story extends React.Component {
             }}>
                 <Head>
                     <title>{RichText.asText(this.props.story.data.title)}</title>
+                    <meta property="og:title" content={RichText.asText(this.props.story.data.title)} />
+                    <meta property="og:image" content={this.props.story.data.cover_image.url} />
+                    <meta property="description" content={RichText.asText(this.props.story.data.description)} />
                 </Head>
                 <Heading fontSize={[4, 5, 6]}>{RichText.asText(this.props.story.data.title)}</Heading>
                 <Text m="10px" fontWeight="500">{this.props.story.data.date_created}</Text>
@@ -49,13 +53,27 @@ export default class Story extends React.Component {
                     ))}
                 </Flex>
                 <Image m="15px" width="50%" src={this.props.story.data.cover_image.url} />
-                <RichText render={this.props.story.data.body} />
+                {this.props.story.data.body1.map(slice => (
+                    slice.slice_type == "html" ?
+                        <Flex sx={{
+                            blockquote: {
+                                fontStyle: "italic",
+                                borderLeft: "5px solid red",
+                                pl: "15px"
+                            },
+                            code: {
+                                bg: "hsl(230, 25%, 18%)",
+                                color: "white",
+                                p: "5px"
+                            }
+                        }} flexDirection="column" dangerouslySetInnerHTML={{ __html: marked(RichText.asText(slice.primary.html)) }} />
+                        : <RichText render={slice.primary.text} />
+                ))}
             </Flex>
         )
     }
     static async getInitialProps(ctx) {
         const response = await Client.getByID(ctx.query.id)
-
         return { story: response }
     }
 }
