a105517cd6795b7b50a75d29053c686e91d8934e
Neel Redkar <neel.redkar@gmail.com>
2025-01-23 12:51:09 -0800

    add callout styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "theme-ui";
-import theme from "../components/theme";
-import Nav from "../components/Navbar";
-import Footer from "../components/Footer";
+import theme from "@components/theme";
+import Nav from "@components/Navbar";
+import Footer from "@components/Footer";
 import { Flex } from "theme-ui";
 import Head from "next/head";
 import { useEffect } from "react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -29,8 +29,6 @@ export default function App({ Component, pageProps }) {
             width: 100%;
             max-width: 100%;
           }
-
-
         `}</style>
       </Flex>
     </ThemeProvider>
